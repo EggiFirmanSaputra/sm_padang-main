71c1a0d3fc264f23652e8e5493782f05b0a63f5f
EggiFirmanSaputra <eggi.firman@raharja.info>
2024-10-03 00:27:44 +0700

    change cors

--- sm_padang-main/middleware/cors.go
+++ sm_padang-main/middleware/cors.go
@@ -1,20 +1,57 @@
 package middleware
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+    "net/http"
+    "github.com/gin-gonic/gin"
 )
 
 func CorsHandler() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Cookie, ngrok-skip-browser-warning")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
-		if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusNoContent)
-		}
-		ctx.Next()
-	}
+    return func(ctx *gin.Context) {
+        origin := ctx.Request.Header.Get("Origin")
+        if origin == "http://localhost:3000/" || origin == "https://padang-v2-master-black.vercel.app/" {
+            ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+        } else {
+            ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Fallback or default origin
+        }
+
+        ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Cookie, ngrok-skip-browser-warning")
+        ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+
+        if ctx.Request.Method == "OPTIONS" {
+            ctx.AbortWithStatus(http.StatusNoContent)
+            return
+        }
+
+        ctx.Next()
+    }
 }
+
+
+
+
+
+
+
+
+
+// package middleware
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func CorsHandler() gin.HandlerFunc {
+// 	return func(ctx *gin.Context) {
+// 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+// 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Cookie, ngrok-skip-browser-warning")
+// 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+// 		if ctx.Request.Method == "OPTIONS" {
+// 			ctx.AbortWithStatus(http.StatusNoContent)
+// 		}
+// 		ctx.Next()
+// 	}
+// }
